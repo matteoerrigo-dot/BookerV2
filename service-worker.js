5e4a95b234dfba68dfbfd5dee6645b953bbdb22b
matteoerrigo-dot <matteo.errigo@gmail.com>
2026-03-16 17:35:23 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,27 @@
-const CACHE_NAME = 'booker-cache-v1';
+const CACHE_NAME = "booker-cache-v1";
+
 const urlsToCache = [
-  './',
-  'index.html'
+  "./",
+  "./index.html",
+  "./manifest.json"
 ];
 
-self.addEventListener('install', event => {
+// Installation
+self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
+// Activation
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Requête
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
